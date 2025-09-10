bba2bee5b867dc25b20c685cb699ec0f29a18856
erdoganakpolat <erdoganakpolat98@gmail.com>
2025-09-10 19:37:28 +0300

    DemoSecurityConfig.java has updated

--- src/main/java/com/springboot/demosecurity/security/DemoSecurityConfig.java
+++ src/main/java/com/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -3,9 +3,11 @@ package com.springboot.demosecurity.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
@@ -35,6 +37,21 @@ public class DemoSecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(Erdogan,Ahmet,Hasan);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain (HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(configer ->
+                configer
+                        .anyRequest().authenticated()
+        )
+         .formLogin(form ->
+                 form
+                         .loginPage("/showMyLoginPage")
+                         .loginProcessingUrl("/authenticateTheUser")
+                         .permitAll()
+         );
+        return http.build();
     }
 }
 
